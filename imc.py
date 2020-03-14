a6026e2df726272a693e42ca6d0125a889b607f4
Lívio Lopes <antonio.lvio760@gmail.com>
2020-03-14 19:50:39 -0300

    atualizando precedencia de potencia

--- imc.py
+++ imc.py
@@ -6,7 +6,7 @@ altura = input("Vamos calcular seu IMC, qual sua altura em métros? ")
 
 massa = input("Qual o seu peso em Quilogramas?")
 
-imc = float(altura)/float(massa)**2
+imc = float(altura)/(float(massa)**2)
 
 if (imc < 18,5)
 	print("Seu indice é ", imc," e você está ABAIXO DO PESO")
@@ -18,5 +18,5 @@ elif (imc > 30 && imc < 34,9)
 	print("Seu indice é ", imc," e você está com OBESIDADE GRA 1")
 elif (imc > 35 && imc < 39,9)
 	print("Seu indice é ", imc," e você está com OBESIDADE GRA 2")
-elif (imc < 40 )
+elif (imc > 40 )
 	print("Seu indice é ", imc," e você está com OBESIDADE GRA 3")
