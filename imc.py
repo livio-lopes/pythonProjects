1cf57c9689fbd8f85ecff564737034b7cc70d5ff
Lívio Lopes <antonio.lvio760@gmail.com>
2020-03-14 20:18:08 -0300

    adicionando ambiente de desenvolvimento e corrigindo sintaxe

--- imc.py
+++ imc.py
@@ -6,17 +6,17 @@ altura = input("Vamos calcular seu IMC, qual sua altura em métros? ")
 
 massa = input("Qual o seu peso em Quilogramas?")
 
-imc = float(altura)/(float(massa)**2)
+imc = float(massa)/(float(altura)**2)
 
-if (imc < 18,5)
+if (imc < 18.5):
 	print("Seu indice é ", imc," e você está ABAIXO DO PESO")
-elif (imc > 18,5 && imc < 24,9)
+elif (imc > 18.5 and imc < 24.9):
 	print("Seu indice é ", imc," e você está com PESO NORMAL")
-elif (imc > 25 && imc < 29,9)
+elif (imc > 25 and imc < 29.9):
 	print("Seu indice é ", imc," e você está com SOBREPESO")
-elif (imc > 30 && imc < 34,9)
+elif (imc > 30 and imc < 34.9):
 	print("Seu indice é ", imc," e você está com OBESIDADE GRA 1")
-elif (imc > 35 && imc < 39,9)
+elif (imc > 35 and imc < 39.9):
 	print("Seu indice é ", imc," e você está com OBESIDADE GRA 2")
-elif (imc > 40 )
+else: 
 	print("Seu indice é ", imc," e você está com OBESIDADE GRA 3")
