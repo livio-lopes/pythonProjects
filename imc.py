d3caab9eba6e5fa0aff416307df7135b4832ae08
Lívio Lopes <antonio.lvio760@gmail.com>
2020-03-16 13:47:15 -0300

    ajustando formatação e implementando operador lógico in

--- imc.py
+++ imc.py
@@ -19,25 +19,43 @@ atual = datetime.now()
 
 nome = input('Olá, sou a IMC 2000, a super calculadora de IMC, quem é você?')
 
-idade = input('Eu nascia a 2000 mil anos atrás, qual a sua idade?')
+idade = input('\nEu nascia a 2000 mil anos atrás, qual a sua idade?')
 
-altura = input("Vamos calcular seu IMC, qual sua altura em métros? ")
+altura = input("\nVamos calcular seu IMC, qual sua altura em métros? ")
 
-massa = input("Qual o seu peso em Quilogramas?")
+massa = input("\nQual o seu peso em Quilogramas?")
 
 imc = float(massa)/(float(altura)**2)
 
 ano_nasci = atual.year - int(idade)
 
 if (imc < 18.5):
-	print(f"{nome} você nasceu em {ano_nasci}, correto? Seu indice é {imc:.2f} e você está ABAIXO DO PESO")
+	print(f"{nome} você nasceu em {ano_nasci}, correto?")
+	if '20' in ano_nasci):
+		print(' Você é tão antigo quanto eu!')
+	print(f'\nSeu indice é {imc:.2f} e você está com ABAIXO DO PESO')
 elif (imc > 18.5 and imc < 24.9):
-	print(f"{nome} você nasceu em {ano_nasci}, correto? Seu indice é {imc:.2f} e você está com PESO NORMAL")
+	print(f"{nome} você nasceu em {ano_nasci}, correto?")
+	if '20' in ano_nasci):
+		print(' Você é tão antigo quanto eu!')
+	print(f'\nSeu indice é {imc:.2f} e você está com PESO NORMAL')
 elif (imc > 25 and imc < 29.9):
-	print(f"{nome} você nasceu em {ano_nasci}, correto? Seu indice é {imc:.2f} e você está com SOBREPESO")
+	print(f"{nome} você nasceu em {ano_nasci}, correto?")
+	if '20' in ano_nasci):
+		print(' Você é tão antigo quanto eu!')
+	print(f'\nSeu indice é {imc:.2f} e você está com SOBREPESO')
 elif (imc > 30 and imc < 34.9):
-	print(f"{nome} você nasceu em {ano_nasci}, correto? Seu indice é {imc:.2f} e você está com OBESIDADE GRA 1")
+	print(f"{nome} você nasceu em {ano_nasci}, correto?")
+	if '20' in ano_nasci):
+		print(' Você é tão antigo quanto eu!')
+	print(f'\nSeu indice é {imc:.2f} e você está com OBESIDADE GRA 2')
 elif (imc > 35 and imc < 39.9):
-	print(f"{nome} você nasceu em {ano_nasci}, correto? Seu indice é {imc:.2f} e você está com OBESIDADE GRA 2")
+	print(f"{nome} você nasceu em {ano_nasci}, correto? ")
+	if '20' in ano_nasci):
+		print(' Você é tão antigo quanto eu!')
+	print(f'\nSeu indice é {imc:.2f} e você está com OBESIDADE GRA 2')
 else: 
-	print(f"{nome} você nasceu em {ano_nasci}, correto? Seu indice é {imc:.2f} e você está com OBESIDADE GRA 3")
+	print(f"{nome} você nasceu em {ano_nasci}, correto? ")
+	if '20' in ano_nasci):
+		print(' Você é tão antigo quanto eu!')
+	print(f'\nSeu indice é {imc:.2f} e você está com OBESIDADE GRA 3')
