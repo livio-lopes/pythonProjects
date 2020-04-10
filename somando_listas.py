54ff45f109abbb7d19028d3652a20a41d8c97c82
Lívio Lopes <antonio.lvio760@gmail.com>
2020-04-10 13:40:05 -0300

    atualizando com solução genérica

--- somando_listas.py
+++ somando_listas.py
@@ -5,8 +5,19 @@
 lista_1 = [1,2,3,4,5,6,7]
 lista_2 = [1,2,3,4]
 
+#Solução em Python
 #Combinando as listas
 soma = zip(lista_1,lista_2)
 #Formando lista de somas dos valores desempacotados
 total = [x + y for x,y in soma]
 print(total)
+
+#Solução genérica
+
+lista_soma = []
+if len(lista_1) >= len(lista_2):
+	for index in range(len(lista_2)):
+		lista_soma.append(lista_1[index] + lista_2[index])
+	print(lista_soma)
+else:
+	pass
