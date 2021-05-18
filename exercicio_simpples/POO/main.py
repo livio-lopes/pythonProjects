54b1d388630933cd6773905461ba5efd1221bb11
Marunga <antonio.lvio760@gmail.com>
2021-05-18 12:39:37 -0300

    POO exemplo classe Pessoa

---
+++ exercicio_simpples/POO/main.py
@@ -0,0 +1,12 @@
+from pessoa import Pessoa
+
+
+#instanciando uma pessoa
+p1=Pessoa('Bob',32)
+p1.comer('Laranja')
+p1.falar('Melão')
+p1.parar_de_comer()
+p1.falar('Melão')
+p1.comer('Laranja')
+p1.parar_de_falar()
+p1.comer('Laranja')
---
+++ exercicio_simpples/POO/pessoa.py
@@ -0,0 +1,53 @@
+
+
+
+
+class Pessoa:
+
+	def __init__(self, nome, idade, comendo=False, falando=False):
+		self.nome = nome
+		self.idade = idade
+		self.comendo = comendo
+		self.falando = falando
+
+	def comer(self, alimento):
+
+		if self.comendo:
+			print(f"{self.nome} já está comendo")
+			return
+
+		if self.falando:
+			print(f'{self.nome} está falando, não pode comer')
+			return
+
+		print(f"{self.nome} está comendo {alimento}")
+		self.comendo = True
+
+	def parar_de_comer(self):
+		if not self.comendo:
+			print(f"{self.nome} não está comendo")
+			return
+
+		print(f"{self.nome} parou de comer")
+		self.comendo = False
+
+	def falar(self,assunto):
+		
+		if self.falando:
+			print(f'{self.nome} já está falando')
+			return
+
+		if self.comendo:
+			print(f'{self.nome} está comendo, não pode falar')
+			return
+
+		print(f'{self.nome} está falando sobre {assunto}')
+		self.falando = True
+
+	def parar_de_falar(self):
+		if not self.falando:
+			print(f'{self.nome} não está falando')
+			return
+
+		print(f'{self.nome} parou de falar')
+		self.falando = False
